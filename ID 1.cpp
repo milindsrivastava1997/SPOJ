bf339e54ef343d653a0f747e693d22aa5d893ac4
milindsrivastava1997 <milindsrivastava1997@gmail.com>
2014-11-16 19:56:09 +0530

    Added ID 1.cpp

---
+++ ID 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	while(true)
+	{
+		cin>>x;
+		cin.ignore();
+		if(x==42)
+		{
+			break;
+		}
+		cout<<x<<endl;
+	}
+	return 0;
+}
